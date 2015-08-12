d313bac7eee339b2aea99ed14f3cf22f695b839c
Gladion <Gladion@10.61.66.103>
2015-08-12 14:05:00 +0800

    Updated  @  back image right left div home page

--- script/engine.js
+++ script/engine.js
@@ -9,13 +9,18 @@ $(function() {
             * @Element     : img
             * @Description : Wait for images to load then fade them In
             */
-           $('.wait img').load(function (event) {
-               
+           $('.wait img').load(function () {
                 setTimeout(function(){
                             $(event.target).fadeIn(1500); 
                                      }, 5000);
            });
-                   
+           /**
+            * @class gen-sub
+            * @description : Activate loading
+            */
+           $('.gen_sub').click(function(){
+               $('.loader').show();
+           });
               
                        
 });
