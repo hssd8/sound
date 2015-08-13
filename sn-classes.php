b6cb9f0a44d206fb4dfe33017f545212a4eb78ba
Ibrahim Isa <hssd8@yahoo.com>
2015-08-14 04:18:41 +0800

    New file added

---
+++ sn-classes.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Classes file
+ * 
+ * This file is important to all pages changes can be made to constants if  the 
+ * need arises
+ * @author Ibrahim & Atiku 
+ * @version 1.0
+ * @link http://www.github.com/hssd8/sound
+ */
+?>
+
---
+++ sn-functions.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * function file
+ * 
+ * This file is important to all pages changes can be made to constants if  the 
+ * need arises
+ * @author Ibrahim & Atiku 
+ * @version 1.0
+ * @link http://www.github.com/hssd8/sound
+ */
+
+
